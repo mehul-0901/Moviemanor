d79ec35565464eb1a3fa94f4cfd6dfd71d25e635
Mehul Savaliya <mehulsavaliya520@gmail.com>
2022-05-07 17:53:55 -0400

    Upvote DownVote Front-end

--- client/src/queries.js
+++ client/src/queries.js
@@ -120,8 +120,22 @@ query ListOfComments($movieId: String) {
       comment
       like
       dislike
+      id
     }
   }
+}
+`;
+
+const ADD_LIKE = gql`
+mutation AddLike($movieID: String, $commentID: String, $emailID: String) {
+  addLike(movieID: $movieID, commentID: $commentID, emailID: $emailID)
+}
+
+`;
+
+const ADD_DISLIKE = gql`
+mutation AddDislike($movieID: String, $commentID: String, $emailID: String) {
+  addDislike(movieID: $movieID, commentID: $commentID, emailID: $emailID)
 }
 `;
 
@@ -143,7 +157,9 @@ let exported = {
   Get_Movies_By_IDS,
   GET_MOOD_BASED_MOVIES,
   ADD_COMMENT,
-  SHOW_COMMENTS
+  SHOW_COMMENTS,
+  ADD_LIKE,
+  ADD_DISLIKE
   };
   
 export default exported
